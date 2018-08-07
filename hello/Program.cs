57c3632d7725439d85d7106414347cc82a79b36f
Manuel Peise <manuel.p80@gmx.de>
2018-08-07 21:44:18 +0200

    console application

---
+++ hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine().ToString();
+
+            Console.WriteLine("Hello " + name + "!");
+
+
+
+        }
+    }
+}
